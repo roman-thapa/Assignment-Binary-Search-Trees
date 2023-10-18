4188b9f4a50aa0c9bbf462818b5ade10300bedaf
roman <51534566+roman-thapa@users.noreply.github.com>
2023-10-18 09:50:51 +0530

    Make a class constructor to create node and BST

---
+++ main.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(value) {
+      this.value = value
+      this.left = null
+      this.right = null
+    }
+}
+  
+class BinarySearchTree {
+    constructor() {
+      this.root = null
+    }
+    
+    isEmpty() {
+      return this.root === null
+    }
+    
+    insert(data) {
+      let newNode = new Node(data)
+      if(this.isEmpty()) {
+        this.root = newNode
+      }else {
+        this.insertNode(this.root, newNode)
+      }
+    }
+    
+    insertNode(root, newNode){
+        if(newNode.value < root.value) {
+            if(root.left===null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        } else {
+            if(root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+}
+  
+const bst = new BinarySearchTree()
+
+console.log(bst.isEmpty())
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)

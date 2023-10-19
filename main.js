cf46cf292c0305a9eef47a65a165cecc211eabf9
roman <51534566+roman-thapa@users.noreply.github.com>
2023-10-19 09:52:31 +0530

    Add a search method to search if a value exist

--- main.js
+++ main.js
@@ -1,44 +1,58 @@
 class Node {
-    constructor(value) {
-      this.value = value
-      this.left = null
-      this.right = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
-  
+
 class BinarySearchTree {
-    constructor() {
-      this.root = null
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(data) {
+    let newNode = new Node(data);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
     }
-    
-    isEmpty() {
-      return this.root === null
-    }
-    
-    insert(data) {
-      let newNode = new Node(data)
-      if(this.isEmpty()) {
-        this.root = newNode
-      }else {
-        this.insertNode(this.root, newNode)
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
       }
     }
-    
-    insertNode(root, newNode){
-        if(newNode.value < root.value) {
-            if(root.left===null) {
-                root.left = newNode
-            } else {
-                this.insertNode(root.left, newNode)
-            }
-        } else {
-            if(root.right === null) {
-                root.right = newNode
-            } else {
-                this.insertNode(root.right, newNode)
-            }
-        }
+  }
+
+  search(root, value) {
+    if(!root) {
+      return false;
+    } else {
+      if(root.value===value) {
+        return true
+      } else if(root.value<value) {
+        return this.search(root.right, value)
+      } else {
+        return this.search(root.left, value)
+      }
     }
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -54,11 +68,17 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const bst = new BinarySearchTree()
+const bst = new BinarySearchTree();
 
-console.log(bst.isEmpty())
-bst.insert(10)
-bst.insert(5)
-bst.insert(15)
+console.log(bst.isEmpty()); //true
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(6);
 
-prettyPrint(bst.root)
+prettyPrint(bst.root); 
+// │   ┌── 15
+// └── 10
+//     │   ┌── 6
+//     └── 5
+console.log(bst.search(bst.root, 6)) // true
